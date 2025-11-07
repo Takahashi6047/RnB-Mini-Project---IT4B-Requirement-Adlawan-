9b5b59e02519059d83f6e64306ae49da892a4919
John Robert G. Adlawan <johnrobert.adlawan@msunaawan.edu.ph>
2025-11-07 11:01:21 +0800

    setup colors

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
       <main className="">
-        Django Bnb
+        <h2 className="text-airbnb">Django Bnb</h2>
       </main>
  
   );
