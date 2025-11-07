49edd263106a4ec8536193ac741f6adfa2aa5e1a
John Robert G. Adlawan <johnrobert.adlawan@msunaawan.edu.ph>
2025-11-07 13:54:21 +0800

    property list items static

--- app/components/properties/propertylistitems.tsx
+++ app/components/properties/propertylistitems.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
+
 const PropertyListItems = () => {
+
     return (
-        <div>
-            asdsad
+        <div className="cursor-pointer">
+            <div className="relative overflow-hidden aspect-square rounded-xl">
+                 <Image
+                    fill
+                    src="/propertyItemImages/beach_1.jpg"
+                    sizes="(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
+                    className="hover:scale-110 object-cover transition h-full w-full"
+                    alt="Beach house"
+                />
+            </div>
+
+            <div className="mt-2">
+                <p className="text-lg font-bold">Property Name</p>
+            </div>
+            
+            <div className="mt-2">
+                <p className="text-sm text-gray-500">
+                    <strong>$100</strong> per night
+                </p>
+            </div>
         </div>
     )
 }
