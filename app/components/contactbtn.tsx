de2c5f1753e8a20dc3decfa05eb9fc4cf1fa72df
John Robert G. Adlawan <johnrobert.adlawan@msunaawan.edu.ph>
2025-11-07 19:43:03 +0800

    landlord page static

---
+++ app/components/contactbtn.tsx
@@ -0,0 +1,9 @@
+const ContactBtn = () => {
+    return (
+        <button className="mt-6 py-4 px-6 cursor-pointer bg-airbnb text-white rounded-xl hover:bg-airbnb-dark transition">
+            Contact
+         </button>
+    ) 
+}
+
+export default ContactBtn
