3b61fa2b894d7eb7c24134bbcfae03d2b7bab328
John Robert G. Adlawan <johnrobert.adlawan@msunaawan.edu.ph>
2025-11-08 13:49:50 +0800

    create reusable modal window

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/navbar";
+import Modal from "./components/modals/modal";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,12 @@ export default function RootLayout({
           {children}
         </div>
 
+        <Modal 
+            label="Login"
+            content={<div>Login</div>}
+            isOpen={false}
+            close={() => {}}
+        />
       </body>
     </html>
   );
