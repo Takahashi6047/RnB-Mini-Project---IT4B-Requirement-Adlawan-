61599eebe49c071af4497f48abf2b31d2b1cce56
John Robert G. Adlawan <johnrobert.adlawan@msunaawan.edu.ph>
2025-11-24 17:40:13 +0800

    vid 3: get properties in the frontend and list them (Robert Adlawan)

--- app/components/properties/propertylist.tsx
+++ app/components/properties/propertylist.tsx
@@ -1,11 +1,54 @@
+'use client';
+
+
+import { useEffect, useState } from 'react';
 import PropertyListItems from "./propertylistitems"
 
+export type PropertyType = {
+    id: string;
+    title: string;
+    image_url: string;
+    price_per_night: number;
+    is_favorite: boolean;
+}
+
 const PropertyList = () => {
+    const [properties, setProperties] = useState<PropertyType[]>([]);
+    const getProperties = async () => {
+        let url = 'http://localhost:8000/api/properties';
+
+        await fetch(url, {
+            method: 'GET',
+        })
+            .then(response => response.json())
+            .then((json) => {
+                console.log('json', json)
+
+                setProperties(json.data)
+            })
+            .catch((error) => {
+                console.log('error', error)
+            })
+    };    
+
+
+    useEffect(() => {
+        getProperties();
+    }, []);
+
+
     return (
         <>
-            <PropertyListItems />
-            <PropertyListItems />
-            <PropertyListItems />
+            {properties.map((property) =>  {
+                return (
+                    <PropertyListItems 
+                        key={property.id} 
+                        property={property}
+                    />
+                )
+            })}
+            
+            
        </>
     )
 }
--- app/components/properties/propertylistitems.tsx
+++ app/components/properties/propertylistitems.tsx
@@ -1,13 +1,20 @@
 import Image from "next/image";
+import { PropertyType } from "./propertylist";
 
-const PropertyListItems = () => {
+interface PropertyProps {
+    property: PropertyType,
+}
+
+const PropertyListItems: React.FC<PropertyProps> = ({
+    property,
+}) => {
 
     return (
         <div className="cursor-pointer">
             <div className="relative overflow-hidden aspect-square rounded-xl">
                  <Image
                     fill
-                    src="/propertyItemImages/beach_1.jpg"
+                    src={property.image_url}
                     sizes="(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
                     className="hover:scale-110 object-cover transition h-full w-full"
                     alt="Beach house"
@@ -15,14 +22,13 @@ const PropertyListItems = () => {
             </div>
 
             <div className="mt-2">
-                <p className="text-lg font-bold">Property Name</p>
+                <p className="text-lg font-bold">{property.title}</p>
             </div>
             
-            <div className="mt-2">
-                <p className="text-sm text-gray-500">
-                    <strong>$100</strong> per night
-                </p>
-            </div>
+           <p className="text-sm text-gray-500">
+                <strong>${property.price_per_night} </strong>
+                per night
+             </p>
         </div>
     )
 }
