b42dbe76beae7318d9e92be545284a74d55e8ee1
John Robert G. Adlawan <johnrobert.adlawan@msunaawan.edu.ph>
2025-11-26 01:48:56 +0800

    v4 sign up done (robert adlawan)

--- app/components/modals/SignUpModal.tsx
+++ app/components/modals/SignUpModal.tsx
@@ -2,32 +2,74 @@
 
 import Modal from "./Modal";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import useSignUpModal from "@/app/hooks/useSignUpModal";
 import CustomBtn from "../forms/CustomBtn";
+import apiService from "@/app/services/apiService";
+import { handleLogin } from "@/app/lib/actions";
+
 
 const SignUpModal = () => {
-    const signUpModal = useSignUpModal()
+    const router = useRouter();
+    const signUpModal = useSignUpModal();
+    const [email, setEmail] = useState('');
+    const [password1, setPassword1] = useState('');
+    const [password2, setPassword2] = useState('');
+    const [errors, setErrors] = useState<string[]>([]);
+
+    const submitSignup = async () => {
+        const formData = {
+            email: email,
+            password1: password1,
+            password2: password2
+        }
+
+            const response = await apiService.post('/api/auth/register/', JSON.stringify(formData));
+
+
+        if (response.access) {
+            handleLogin(response.user.pk, response.access, response.refresh);
+            
+            signUpModal.close();
+
+            router.push('/')
+        } else {
+            const tmpErrors: string[] = Object.values(response).map((error: any) => {
+                return error;
+            })
+
+            setErrors(tmpErrors);
+        }
+    }
 
     const content = (
         <>
-            <h2 className="mb-6 text-2xl">welcome djangobnb sign up</h2>
+            <h2 className="mb-6 text-2xl"> Sign up</h2>
 
             <form 
+                action={submitSignup}
                 className="space-y-4"
             >
-                <input placeholder="Your e-mail address" type="email" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
+                <input onChange={(e) => setEmail(e.target.value)} placeholder="Your e-mail address" type="email" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
 
-                <input placeholder="Your password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
-                <input placeholder="Repeat password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
-                <div 
-                    className="p-5 bg-airbnb text-white rounded-xl opacity-80"
-                >
-                    ERROR
-                </div>
+                <input onChange={(e) => setPassword1(e.target.value)} placeholder="Your password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
+
+                <input onChange={(e) => setPassword2(e.target.value)} placeholder="Repeat password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
+
+                {errors.map((error, index) => {
+                    return (
+                        <div 
+                            key={`error_${index}`}
+                            className="p-5 bg-airbnb text-white rounded-xl opacity-80"
+                        >
+                            {error}
+                        </div>
+                    )
+                })}
                
                 <CustomBtn
                     label="Submit"
-                    onClick={() => {}}
+                    onClick={submitSignup}
                 />
             </form>
         </>
