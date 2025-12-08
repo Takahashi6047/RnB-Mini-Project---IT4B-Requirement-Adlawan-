176c1e26109472acf00e4b3439868a6fb34f1f0e
Robert Adlawan <johnrobert.adlawan@msunaawan.edu.ph>
2025-12-08 15:22:18 +0800

    v8 choose between categories in frontpage (Robert Adlawan)

--- app/components/properties/propertylist.tsx
+++ app/components/properties/propertylist.tsx
@@ -1,9 +1,11 @@
 'use client';
 
-
+import { format } from 'date-fns';
 import { useEffect, useState } from 'react';
-import PropertyListItems from "./propertylistitems"
+import { useSearchParams } from 'next/navigation';
+import PropertyListItem from "./propertylistitems";
 import apiService from '@/app/services/apiService';
+import useSearchModal from '@/app/hooks/useSearchModal';
 
 export type PropertyType = {
     id: string;
@@ -22,8 +24,19 @@ const PropertyList: React.FC<PropertyListProps> = ({
     landlord_id,
     favorites
 }) => {
-
+    const params = useSearchParams();
+    const searchModal = useSearchModal();
+    const country = searchModal.query.country;
+    const numGuests = searchModal.query.guests;
+    const numBathrooms = searchModal.query.bathrooms;
+    const numBedrooms = searchModal.query.bedrooms;
+    const checkinDate = searchModal.query.checkIn;
+    const checkoutDate = searchModal.query.checkOut;
+    const category = searchModal.query.category;
     const [properties, setProperties] = useState<PropertyType[]>([]);
+
+    console.log('searchQUery:', searchModal.query);
+    console.log('numBedrooms', numBedrooms)
 
     const markFavorite = (id: string, is_favorite: boolean) => {
         const tmpProperties = properties.map((property: PropertyType) => {
@@ -50,6 +63,44 @@ const PropertyList: React.FC<PropertyListProps> = ({
             url += `?landlord_id=${landlord_id}`
         } else if (favorites) {
             url += '?is_favorites=true'
+        } else {
+            let urlQuery = '';
+
+            if (country) {
+                urlQuery += '&country=' + country
+            }
+
+            if (numGuests) {
+                urlQuery += '&numGuests=' + numGuests
+            }
+
+            if (numBedrooms) {
+                urlQuery += '&numBedrooms=' + numBedrooms
+            }
+
+            if (numBathrooms) {
+                urlQuery += '&numBathrooms=' + numBathrooms
+            }
+
+            if (category) {
+                urlQuery += '&category=' + category
+            }
+
+            if (checkinDate) {
+                urlQuery += '&checkin=' + format(checkinDate, 'yyyy-MM-dd')
+            }
+
+            if (checkoutDate) {
+                urlQuery += '&checkout=' + format(checkoutDate, 'yyyy-MM-dd')
+            }
+
+            if (urlQuery.length) {
+                console.log('Query:', urlQuery);
+
+                urlQuery = '?' + urlQuery.substring(1);
+
+                url += urlQuery;
+            }
         }
 
         const tmpProperties = await apiService.get(url)
@@ -65,27 +116,23 @@ const PropertyList: React.FC<PropertyListProps> = ({
         }));
     };
 
-
     useEffect(() => {
         getProperties();
-    }, []);
-
+    }, [category, searchModal.query, params]);
 
     return (
         <>
             {properties.map((property) => {
                 return (
-                    <PropertyListItems
+                    <PropertyListItem
                         key={property.id}
                         property={property}
                         markFavorite={(is_favorite: any) => markFavorite(property.id, is_favorite)}
                     />
                 )
             })}
-
-
         </>
     )
 }
 
-export default PropertyList
+export default PropertyList;
